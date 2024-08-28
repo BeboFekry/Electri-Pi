e2bcba8eb0946b2786581daf9e279f9bc09b3834
Abdallah Fekry <116501243+BeboFekry@users.noreply.github.com>
2024-08-28 13:16:42 +0300

    Update langchain.py

--- langchain.py
+++ langchain.py
@@ -5,6 +5,17 @@ from langchain_core.prompts.prompt import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from langchain_google_genai import GoogleGenerativeAI
 from langchain_core.runnables import RunnablePassthrough
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
 from langchain_chroma import Chroma
 from pathlib import Path
 import gdown
