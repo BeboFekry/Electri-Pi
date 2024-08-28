bd3053adfde5f5f550413dcff5486db01f10b2d1
Abdallah Fekry <116501243+BeboFekry@users.noreply.github.com>
2024-08-28 15:50:54 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,35 +20,35 @@ class embedding:
     def embed_query(self, query):
         return self.model.encode(query).tolist()
 
-    file_id = '1-LspPqw7CET-euOzI8A3L60SQA3CQo9o'
-    url = f'https://drive.google.com/uc?id={file_id}'
-    output = 'MyVectorDB/chroma.sqlite3'
-    gdown.download(url, output, quiet=False)
+file_id = '1-LspPqw7CET-euOzI8A3L60SQA3CQo9o'
+url = f'https://drive.google.com/uc?id={file_id}'
+output = 'MyVectorDB/chroma.sqlite3'
+gdown.download(url, output, quiet=False)
 
-    file_id = "1-XNpT7b_W6MR-9PyOyQT-4J4fnnzBdjO"
-    url = f'https://drive.google.com/uc?id={file_id}'
-    output = 'MyVectorDB/785d685c-8ac5-4be7-9e32-13b827c88a50/data_level0.bin'
-    gdown.download(url, output, quiet=False)
+file_id = "1-XNpT7b_W6MR-9PyOyQT-4J4fnnzBdjO"
+url = f'https://drive.google.com/uc?id={file_id}'
+output = 'MyVectorDB/785d685c-8ac5-4be7-9e32-13b827c88a50/data_level0.bin'
+gdown.download(url, output, quiet=False)
 
-    file_id = "1-cYtH2qkE-3ouIAZuTKsprLo00i0WEiI"
-    url = f'https://drive.google.com/uc?id={file_id}'
-    output = 'MyVectorDB/785d685c-8ac5-4be7-9e32-13b827c88a50/header.bin'
-    gdown.download(url, output, quiet=False)
+file_id = "1-cYtH2qkE-3ouIAZuTKsprLo00i0WEiI"
+url = f'https://drive.google.com/uc?id={file_id}'
+output = 'MyVectorDB/785d685c-8ac5-4be7-9e32-13b827c88a50/header.bin'
+gdown.download(url, output, quiet=False)
 
-    file_id = "1-QpaRSHPlF-6GYivut1HGe9L0Ec-9pAi"
-    url = f'https://drive.google.com/uc?id={file_id}'
-    output = 'MyVectorDB/785d685c-8ac5-4be7-9e32-13b827c88a50/index_metadata.pickle'
-    gdown.download(url, output, quiet=False)
+file_id = "1-QpaRSHPlF-6GYivut1HGe9L0Ec-9pAi"
+url = f'https://drive.google.com/uc?id={file_id}'
+output = 'MyVectorDB/785d685c-8ac5-4be7-9e32-13b827c88a50/index_metadata.pickle'
+gdown.download(url, output, quiet=False)
 
-    file_id = "1-jwYjTVXbJT75Kvme70EWosCfkfV8p__"
-    url = f'https://drive.google.com/uc?id={file_id}'
-    output = 'MyVectorDB/785d685c-8ac5-4be7-9e32-13b827c88a50/length.bin'
-    gdown.download(url, output, quiet=False)
+file_id = "1-jwYjTVXbJT75Kvme70EWosCfkfV8p__"
+url = f'https://drive.google.com/uc?id={file_id}'
+output = 'MyVectorDB/785d685c-8ac5-4be7-9e32-13b827c88a50/length.bin'
+gdown.download(url, output, quiet=False)
 
-    file_id = "1-jYB22FMgoYND6D34Rp2mHDsT_C6YL5N"
-    url = f'https://drive.google.com/uc?id={file_id}'
-    output = 'MyVectorDB/785d685c-8ac5-4be7-9e32-13b827c88a50/link_lists.bin'
-    gdown.download(url, output, quiet=False)
+file_id = "1-jYB22FMgoYND6D34Rp2mHDsT_C6YL5N"
+url = f'https://drive.google.com/uc?id={file_id}'
+output = 'MyVectorDB/785d685c-8ac5-4be7-9e32-13b827c88a50/link_lists.bin'
+gdown.download(url, output, quiet=False)
 
 embed_model = embedding()
 
